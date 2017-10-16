0d85ec2537b0571c1179b9298f68cee012a2d85e
Geo <gaa39@mail.aub.edu>
2017-10-16 21:36:31 +0300

    added graphs for Apps and Features

--- CrowdApp/dashboard/data_logic.php
+++ CrowdApp/dashboard/data_logic.php
@@ -19,16 +19,30 @@ try{
 		exit();
 	}
 
-	$chart_type = $_POST["c_type"];
+	$c_type = $_POST["c_type"];
 
 	if(isset($_POST["filters"])){
-		$filters = $_POST["filters"];	
+		$filters = json_decode($_POST["filters"]);	
 	}
 
 
 	$sql='';
 	$view_res = null;
-	switch ($chart_type) {
+	switch ($c_type) {
+		case 00:
+		$sql = "select distinct a.app_id,a.app_name from user_logs_counter ulc join user_apps ua on ua.ua_id = ulc.user_app_id join Apps a on a.app_id = ua.app_id";
+		$apps_with_logs = mysqli_query($con,$sql);
+		if($apps_with_logs && $apps_with_logs->num_rows > 0){
+			$apps = array();
+			while($apps_with_logs_res = mysqli_fetch_row($apps_with_logs)){
+				array_push($apps, array('id' => $apps_with_logs_res[0],'name' => $apps_with_logs_res[1]));
+			}
+
+			echo json_encode(array('status' => 'Success', 'apps'=> $apps));
+			mysqli_close($con);
+			exit();
+		}
+		break;
 		case 1:
 		$sql = "SELECT active,count(*) as count FROM `Users` GROUP by active";
 		$view = mysqli_query($con,$sql);
@@ -48,8 +62,8 @@ try{
 		$count = array();
 		if($view && $view->num_rows > 0){
 			while($view_res = mysqli_fetch_row($view)){
-				 array_push($category, $view_res[0]);
-				 array_push($count, $view_res[1]+0);
+				array_push($category, $view_res[0]);
+				array_push($count, $view_res[1]+0);
 			}
 			echo json_encode(array('status' => 'Success', 'data'=> array('category' => $category, 'count' => $count)));
 			mysqli_close($con);
@@ -64,8 +78,8 @@ try{
 		$count = array();
 		if($view && $view->num_rows > 0){
 			while($view_res = mysqli_fetch_row($view)){
-				 array_push($category, $view_res[0]);
-				 array_push($count, $view_res[1]+0);
+				array_push($category, $view_res[0]);
+				array_push($count, $view_res[1]+0);
 			}
 			echo json_encode(array('status' => 'Success', 'data'=> array('category' => $category, 'count' => $count)));
 			mysqli_close($con);
@@ -75,23 +89,23 @@ try{
 
 		case 4:
 		$sql = "select t.reputation as `range`, count(*) as `users_count`  
-				from (  
-				  select case    
-				    when `user_reputation` >= 0 and `user_reputation` < 0.2 then ' 0-0.2'  
-				    when `user_reputation` >= 0.2 and `user_reputation` < 0.4 then ' 0.2-0.4'  
-				    when `user_reputation` >= 0.4 and `user_reputation` < 0.6 then ' 0.4-0.6'  
-				    when `user_reputation` >= 0.6 and `user_reputation` < 0.8 then ' 0.6-0.8'  
-				    when `user_reputation` >= 0.8 and `user_reputation` <= 1 then ' 0.8-1'  
-				     end as reputation  
-				  from Users) t  
-				group by t.reputation";
+		from (  
+		select case    
+		when `user_reputation` >= 0 and `user_reputation` < 0.2 then ' 0-0.2'  
+		when `user_reputation` >= 0.2 and `user_reputation` < 0.4 then ' 0.2-0.4'  
+		when `user_reputation` >= 0.4 and `user_reputation` < 0.6 then ' 0.4-0.6'  
+		when `user_reputation` >= 0.6 and `user_reputation` < 0.8 then ' 0.6-0.8'  
+		when `user_reputation` >= 0.8 and `user_reputation` <= 1 then ' 0.8-1'  
+		end as reputation  
+		from Users) t  
+		group by t.reputation";
 		$view = mysqli_query($con,$sql);
 		$category = array();
 		$count = array();
 		if($view && $view->num_rows > 0){
 			while($view_res = mysqli_fetch_row($view)){
-				 array_push($category, $view_res[0]);
-				 array_push($count, $view_res[1]+0);
+				array_push($category, $view_res[0]);
+				array_push($count, $view_res[1]+0);
 			}
 			echo json_encode(array('status' => 'Success', 'data'=> array('category' => $category, 'count' => $count)));
 			mysqli_close($con);
@@ -99,17 +113,151 @@ try{
 		}
 		break;
 
-		default:
-		# code...
+		case 5:
+		$sql = "SELECT `feature_name`,`average_value`,`std_value` FROM `features`";
+		$view = mysqli_query($con,$sql);
+		$category = array();
+		$average = array();
+		$std = array();
+		if($view && $view->num_rows > 0){
+			while($view_res = mysqli_fetch_row($view)){
+				if($view_res[0] == "prRSS" || $view_res[0] == "prVSS" || $view_res[0] == "txBytes" || $view_res[0] == "rxBytes" ){
+					array_push($category, $view_res[0]."*10^-3");
+					array_push($average, ($view_res[1]+0.0)/1000);
+					array_push($std, ($view_res[2]+0.0)/1000);
+				}else{
+					array_push($category, $view_res[0]);
+					array_push($average, $view_res[1]+0.0);
+					array_push($std, $view_res[2]+0.0);
+				}
+
+			}
+			echo json_encode(array('status' => 'Success', 'data'=> array('category' => $category, 'average' => $average, 'std' => $std)));
+			mysqli_close($con);
+			exit();
+		}
 		break;
+
+		case 6:
+		$sql = "SELECT ft.feature_name,count(*) FROM `user_logs_counter` ul join user_apps ua on ul.user_app_id = ua.ua_id join features ft on ft.ft_id = ul.feature_id group by ft.feature_name";
+		$view = mysqli_query($con,$sql);
+		$category = array();
+		$count = array();
+		if($view && $view->num_rows > 0){
+			while($view_res = mysqli_fetch_row($view)){
+
+				array_push($category, $view_res[0]);
+				array_push($count, $view_res[1]+0.0);
+
+
+			}
+			echo json_encode(array('status' => 'Success', 'data'=> array('category' => $category, 'count' => $count)));
+			mysqli_close($con);
+			exit();
+		}
+		break;
+
+		case 7:
+
+		$sql = "SELECT DISTINCT ft_id,feature_name from features";
+		$features = mysqli_query($con,$sql);
+		$features_series = array();
+		if($features && $features->num_rows > 0){
+			while($features_res = mysqli_fetch_row($features)){
+				$sql_i = "SELECT CONCAT(UNIX_TIMESTAMP(time),'000'), cast(case when feature_id in(3,4,5,6) then feature_avg/1000 when feature_id in (1,2,7,8,11) then feature_avg end as decimal(20,2))  from features_tracking WHERE feature_id = $features_res[0]";
+				$feature_vals = mysqli_query($con,$sql_i);
+				if($feature_vals && $feature_vals->num_rows > 0){
+					if($features_res[1] == "prRSS" || $features_res[1] == "prVSS" || $features_res[1] == "txBytes" || $features_res[1] == "rxBytes" ){
+						$app_name = $features_res[1].'*10^-3';
+					}else{
+						$app_name = $features_res[1];
+					}
+					
+					$app_series = array();
+					while($feaures_vals_res = mysqli_fetch_row($feature_vals)){
+						array_push($app_series, array($feaures_vals_res[0]+0,$feaures_vals_res[1]+0));
+					}
+					array_push($features_series, array("name" => $app_name, "data" => $app_series));
+				} 
+
+			}
+			echo json_encode(array('status' => 'Success', 'data'=> $features_series));
+			mysqli_close($con);
+			exit();
+		}
+		break;
+
+		case 8:
+		$sql = "select case when app_name is null then package_name else app_name end as app, app_reputation_normalized as reputation from Apps";
+		$apps = mysqli_query($con,$sql);
+		$appsArray = array();
+		$reputation = array();
+		if($apps && $apps->num_rows > 0){
+			while($apps_res = mysqli_fetch_row($apps)){
+				array_push($appsArray, $apps_res[0]);
+				array_push($reputation, $apps_res[1]+0.0);
+			}
+			echo json_encode(array('status' => 'Success', 'data'=> array('apps' => $appsArray, 'reputation' => $reputation)));
+			mysqli_close($con);
+			exit();
+		}
+		break;
+
+
+		case 9:
+
+		$sql = "SELECT case when a.app_name is null then a.package_name else a.app_name end as name ,count(*) FROM `user_logs_counter` ul join user_apps ua on ul.user_app_id = ua.ua_id join Apps a on a.app_id = ua.app_id group by name";
+		$apps = mysqli_query($con,$sql);
+		$appsArray = array();
+		$logsCount = array();
+		if($apps && $apps->num_rows > 0){
+			while($apps_res = mysqli_fetch_row($apps)){
+				array_push($appsArray, $apps_res[0]);
+				array_push($logsCount, $apps_res[1]+0.0);
+			}
+			echo json_encode(array('status' => 'Success', 'data'=> array('apps' => $appsArray, 'logscount' => $logsCount)));
+			mysqli_close($con);
+			exit();
+		}
+
+		break;
+
+		case 10:
+		$sql = "SELECT DISTINCT ft_id,feature_name from features";
+		$features = mysqli_query($con,$sql);
+		$features_series = array();
+		if($features && $features->num_rows > 0){
+			while($features_res = mysqli_fetch_row($features)){
+				$sql_i = "SELECT feature_id,concat(UNIX_TIMESTAMP(`timestamp`),'000') as time,count(*) FROM `user_logs_counter` ul join user_apps ua on ul.user_app_id = ua.ua_id join Apps a on a.app_id = ua.app_id where a.app_id = $filters and feature_id = $features_res[0] group by time";
+				$feature_vals = mysqli_query($con,$sql_i);
+				if($feature_vals && $feature_vals->num_rows > 0){
+					$feature_series = array();
+					while ($feature_vals_res = mysqli_fetch_row($feature_vals)) {
+						array_push($feature_series, array($feature_vals_res[1]+0,$feature_vals_res[2]+0));
+					}
+					array_push($features_series, array("name" => $features_res[1], "data" => $feature_series));
+				}
+			}
+			echo json_encode(array('status' => 'Success', 'data'=> $features_series));
+			mysqli_close($con);
+			exit();
+		}
+		break;
+
+		default:
+			echo json_encode(array('status' => 'Error', 'message'=> "No such chart"));
+			mysqli_close($con);
+			exit();
+		break;
+
 	}
 
-	echo json_encode(array('status' => 'Error t'));
+	echo json_encode(array('status' => 'NoData'));
 	mysqli_close($con);
 	exit();
 
 }catch(Exception $e){
-	echo json_encode(array('status' => 'Error c'));
+	echo json_encode(array('status' => 'Error','message' => $e->getMessage()));
 	mysqli_close($con);
 	exit();
 }
--- CrowdApp/dashboard/main.php
+++ CrowdApp/dashboard/main.php
@@ -26,14 +26,14 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
 <html>
 <head>
 	<title>CrowdApp Dashboard</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-	<script src="https://code.highcharts.com/stock/highstock.js"></script>
-	<script src="https://code.highcharts.com/highcharts.js"></script>
-	<script src="https://code.highcharts.com/modules/exporting.js"></script>
-	<script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
+	<script src="./js/highstock.js"></script>
+
+	<script src="./js/modules/exporting.js"></script>
+	
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
@@ -111,7 +111,7 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
 				}
 			}).fail(function( xhr, status, errorThrown ){
 				//return null;
-				$('#container1').html("Data Not Found");
+				$('#container2').html("Data Not Found");
 			}).always(function() {
 				return null;
 			});
@@ -150,7 +150,7 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
 				}
 			}).fail(function( xhr, status, errorThrown ){
 				//return null;
-				$('#container1').html("Data Not Found");
+				$('#container3').html("Data Not Found");
 			}).always(function() {
 				return null;
 			});
@@ -189,20 +189,339 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
 				}
 			}).fail(function( xhr, status, errorThrown ){
 				//return null;
-				$('#container1').html("Data Not Found");
+				$('#container4').html("Data Not Found");
 			}).always(function() {
 				return null;
 			});
 		}
-		$( document ).ready(function() {
 
-			getActiveUsers();
-			getUsersOS();
-			getUsersKnowledge();
-			getUsersReputation();
+		function getFeatureAverages(){
+			$.ajax({
+				url: "data_logic.php",
+				type: "POST",
+				data:{"c_type":5}
+			}).done(function (result) {
+				//console.log(result.data);
+				if(result.status == "Success"){
+					$('#container5').highcharts({
+						chart: {
+							type: 'column'
+						},
+						title: {
+							text: 'Feature stats'
+						},
+						xAxis: {
+							categories: result.data.category
+						},
+						yAxis: {
+							title: {
+								text: 'Value'
+							}
+						},
+						series: [{
+							name: "Average",
+							data: result.data.average
+						},{
+							name: "Standard Deviation",
+							data: result.data.std
+						}]
+					});
+				}else{
+					return null;
+				}
+			}).fail(function( xhr, status, errorThrown ){
+				//return null;
+				$('#container5').html("Data Not Found");
+			}).always(function() {
+				return null;
+			});
+		}
 
-		});
-	</script>
+		function getFeaturesLogCount(){
+			$.ajax({
+				url: "data_logic.php",
+				type: "POST",
+				data:{"c_type":6}
+			}).done(function (result) {
+				//console.log(result.data);
+				if(result.status == "Success"){
+					$('#container6').highcharts({
+						chart: {
+							type: 'column'
+						},
+						title: {
+							text: 'Feature stats'
+						},
+						xAxis: {
+							categories: result.data.category
+						},
+						yAxis: {
+							title: {
+								text: 'Count'
+							}
+						},
+						series: [{
+							name: "Count",
+							data: result.data.count
+						}]
+					});
+				}else{
+					return null;
+				}
+			}).fail(function( xhr, status, errorThrown ){
+				//return null;
+				$('#container6').html("Data Not Found");
+			}).always(function() {
+				return null;
+			});
+		}
+		function getFeaturesAveragesTracking(){
+			$.ajax({
+				url: "data_logic.php",
+				type: "POST",
+				data:{"c_type":7}
+			}).done(function (result) {
+				if(result.status == "Success"){
+					var seriesOptions = [];
+					$.each(result.data,function(i,inner_data){
+						seriesOptions[i] = {
+							name: inner_data.name,
+							data: inner_data.data
+						}
+					});
+
+
+					Highcharts.stockChart('container7', {
+
+						rangeSelector: {
+							selected: 3
+						},
+
+						title: {
+							text: "Features' tracking"
+						},
+
+						yAxis: {
+							plotLines: [{
+								value: 0,
+								width: 2,
+								color: 'silver'
+							}]
+						},
+
+						plotOptions: {
+							series: {
+								showInNavigator: true
+							}
+						},
+
+						tooltip: {
+							pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b>',
+							valueDecimals: 2,
+							split: true
+						},
+
+						series: seriesOptions
+					});
+				}else{
+					return null;
+				}
+			}).fail(function( xhr, status, errorThrown ){
+				//return null;
+				$('#container7').html("Data Not Found");
+			}).always(function() {
+				return null;
+			});
+		}
+		function getAppsReputationNormalized(){
+			$.ajax({
+				url: "data_logic.php",
+				type: "POST",
+				data:{"c_type":8}
+			}).done(function (result) {
+				//console.log(result.data);
+				if(result.status == "Success"){
+					$('#container8').highcharts({
+						chart: {
+							type: 'column'
+						},
+						title: {
+							text: 'Apps reputation'
+						},
+						xAxis: {
+							categories: result.data.apps
+						},
+						yAxis: {
+							title: {
+								text: 'Value'
+							}
+						},
+						series: [{
+							name: "Reputation",
+							data: result.data.reputation
+						}]
+					});
+				}else{
+					return null;
+				}
+			}).fail(function( xhr, status, errorThrown ){
+				//return null;
+				$('#container8').html("Data Not Found");
+			}).always(function() {
+				return null;
+			});
+		}
+
+		function getAppsLogsCount(){
+			$.ajax({
+				url: "data_logic.php",
+				type: "POST",
+				data:{"c_type":9}
+			}).done(function (result) {
+				//console.log(result.data);
+				if(result.status == "Success"){
+					$('#container9').highcharts({
+						chart: {
+							type: 'column'
+						},
+						title: {
+							text: 'Apps Logs Count'
+						},
+						xAxis: {
+							categories: result.data.apps
+						},
+						yAxis: {
+							title: {
+								text: 'Count'
+							}
+						},
+						series: [{
+							name: "Count",
+							data: result.data.logscount
+						}]
+					});
+				}else{
+					return null;
+				}
+			}).fail(function( xhr, status, errorThrown ){
+				//return null;
+				$('#container9').html("Data Not Found");
+			}).always(function() {
+				return null;
+			});
+		}
+
+
+		function getAppsWithLogsForFilter(element){
+			$.ajax({
+				url: "data_logic.php",
+				type: "POST",
+				data:{"c_type":00}
+			}).done(function(result){
+				if(result.status == "Success"){
+					//console.log(result.apps);
+					//var apps_array = $.parseJSON(result.apps);
+					$(element).empty().append($("<option></option>")
+						.attr("value","default")
+						.text("Please select an app"));
+
+					$.each(result.apps, function(i, item) {
+						$(element).append($("<option></option>")
+							.attr("value",item.id)
+							.text(item.name));
+					});
+
+				}
+
+
+				}).fail(function( xhr, status, errorThrown ){
+					return null;
+				}).always(function(){
+					return null;
+				});
+				}
+
+		function getAppsFeaturesTracking(app_id){
+			console.log(app_id);
+			if(app_id == "default"){
+				$('#container10').html("Data Not Found");
+			}else{
+				$.ajax({
+					url: "data_logic.php",
+					type: "POST",
+					data:{"c_type":10,"filters":app_id}
+				}).done(function (result) {
+					if(result.status == "Success"){
+						var seriesOptions = [];
+
+						$.each(result.data,function(i,inner_data){
+							seriesOptions[i] = {
+								name: inner_data.name,
+								data: inner_data.data
+							}
+						});
+
+						Highcharts.stockChart('container10', {
+
+							rangeSelector: {
+								selected: 3
+							},
+
+							title: {
+								text: "Logs' count tracking"
+							},
+
+							yAxis: {
+								plotLines: [{
+									value: 0,
+									width: 2,
+									color: 'silver'
+								}]
+							},
+
+							plotOptions: {
+								series: {
+									showInNavigator: true
+								}
+							},
+
+							tooltip: {
+								pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b>',
+								valueDecimals: 2,
+								split: true
+							},
+
+							series: seriesOptions
+						});
+					}else{
+						return null;
+					}
+				}).fail(function( xhr, status, errorThrown ){
+
+					$('#container10').html("Data Not Found");
+				}).always(function() {
+					return null;
+				});
+
+		}
+
+	}
+	$( document ).ready(function() {
+
+		getActiveUsers();
+		getUsersOS();
+		getUsersKnowledge();
+		getUsersReputation();
+		getFeatureAverages();
+		getFeaturesLogCount();
+		getFeaturesAveragesTracking();
+		getAppsReputationNormalized();
+		getAppsLogsCount();
+
+		getAppsWithLogsForFilter("#track_logs_app");	
+
+	});
+</script>
 </head>
 <body>
 	<div class="container">
@@ -239,8 +558,88 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
 						</div>
 					</div>
 				</div>
+				
 			</div>
 		</div>
+
+		<div class="panel panel-default">
+			<div class="panel-heading">Features Info</div>
+			<div class="panel-body">
+				<div class="row" style="margin:1%;">
+					<div class="col-sm-6">
+						<div class="panel panel-default">
+							<div class="panel-heading">Current Values</div>
+							<div class="panel-body">
+								<div id="container5"></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="panel panel-default">
+							<div class="panel-heading">Logs Count</div>
+							<div class="panel-body">
+								<div id="container6"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row" style="margin:1%;">
+					<div class="col-sm-12">
+						<div class="panel panel-default">
+							<div class="panel-heading">Track</div>
+							<div class="panel-body">
+								<div id="container7"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="panel panel-default">
+			<div class="panel-heading">Apps Info</div>
+			<div class="panel-body">
+				<div class="row" style="margin:1%;">
+					<div class="col-sm-12">
+						<div class="panel panel-default">
+							<div class="panel-heading">Reputation</div>
+							<div class="panel-body">
+								<div id="container8"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row" style="margin:1%;">
+					<div class="col-sm-12">
+						<div class="panel panel-default">
+							<div class="panel-heading">Logs Count</div>
+							<div class="panel-body">
+								<div id="container9"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row" style="margin:1%;">
+					<div class="col-sm-12">
+						<div class="panel panel-default">
+							<div class="panel-heading">Logs Count Tracking</div>
+							<div class="panel-body">
+								<div>
+									<select id="track_logs_app" name="track_logs_app" onchange="getAppsFeaturesTracking(this.value)">
+										
+									</select>
+								</div>
+								<div id="container10"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
 	</div>
 </body>>
 </html>
